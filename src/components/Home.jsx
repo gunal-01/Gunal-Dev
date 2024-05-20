26d24d784263fd1459e2a301e5732eefe24d5756
Gunal <113603447+gunal-01@users.noreply.github.com>
2024-05-21 02:30:09 +0530

    git commit -m 'imagecommit'

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -39,7 +39,7 @@ const Home = () => {
           <img
             src={MyImage}
             alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full"
+            className="rounded-2xl mx-auto w-2/3 max-w-screen-md"
           />
         </div>
       </div>
