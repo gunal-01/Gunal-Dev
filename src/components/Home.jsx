0a910551b9ca11d2ee55dd116e1e723db5971e3e
Gunal <113603447+gunal-01@users.noreply.github.com>
2024-06-19 14:29:13 +0530

    HomePageChanges

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -15,7 +15,7 @@ const Home = () => {
             I'm a Front-End Developer
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            I am familiar in building and desgining Web-Apps.
+            I am familiar in building and designing Web-Apps.
             Currently, I love to work on web application using technologies like
             React & Tailwind.
           </p>
