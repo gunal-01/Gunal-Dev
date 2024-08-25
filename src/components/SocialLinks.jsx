588047c8e5170b961d7930f87839143575ca7008
gunal-01 <gunalprakash006@gmail.com>
2024-08-25 11:37:09 +0530

    changes-restored!

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -31,7 +31,7 @@ const SocialLinks = () => {
           Mail <HiOutlineMail size={30} />
         </>
       ),
-      href: "mailto:gunalprakash006@gmail.com",
+      href: "https://www.gunalprakash006@gmail.com",
     },
     {
       id: 4,
@@ -47,14 +47,13 @@ const SocialLinks = () => {
   ];
 
   return (
-    <div className="flex flex-col top-[35%] right-0 fixed">
-      {/* Adjusted positioning based on screen size */}
+    <div className="hidden lg:flex flex-col top-[35%] left-0 fixed">
       <ul>
         {links.map(({ id, child, href, style, download }) => (
           <li
             key={id}
             className={
-              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500 lg:ml-0 lg:w-1/4 lg:my-2" +
+              "flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-500" +
               " " +
               style
             }
