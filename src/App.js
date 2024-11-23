baa57b153fa4314a472f05ca34197ce08dd9a6b7
gunal-01 <gunalprakash006@gmail.com>
2024-11-23 09:07:25 +0530

    2.3.1-release

--- src/App.js
+++ src/App.js
@@ -5,8 +5,14 @@ import Home from "./components/Home";
 import NavBar from "./components/NavBar";
 import Portfolio from "./components/Portfolio";
 import SocialLinks from "./components/SocialLinks";
+import ReactGA from 'react-ga';
+import React, {useEffect} from 'react'
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('G-38YFPFRJLR');
+    ReactGA.pageview(window.location.pathname + window.location.search)
+    },[]);
   return (
     <div>
       <NavBar />
@@ -15,7 +21,6 @@ function App() {
       <Portfolio />
       <Experience />
       <Contact />
-
       <SocialLinks />
     </div>
   );
