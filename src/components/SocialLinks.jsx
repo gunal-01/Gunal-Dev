e5ddb151c333c6b8ffd855cf18d1b8d7482bda62
gunal-01 <gunalprakash006@gmail.com>
2025-01-06 13:38:37 +0530

    resume-U

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -51,7 +51,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "https://drive.google.com/file/d/1kw2W576JJKYW3Y7mbBh_gDMl1h0ffYlu/view?usp=sharing",
+      href: "https://drive.google.com/file/d/1I802NCkGkjscW_2MhF4wUaTs1qs0yh2G/view?usp=sharing",
       style: "rounded-br-md",
       download: true,
     },
